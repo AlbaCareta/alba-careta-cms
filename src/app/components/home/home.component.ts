7eb9e88a6bb2d7a96e42b59d30f812ef03940fa8
Jordi Colomé <hola@jordicolome.com>
2022-01-19 17:06:01 +0100

    Build 14

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigate(['/galeria/']).then()
+    this.router.navigate(['/biografia/']).then()
   }
 
   logout() {
